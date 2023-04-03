0279c469fe00dd6b6e342122578fd8195fc9bf1b
Martini <lucas.leite.martini@gmail.com>
2023-04-03 15:16:00 -0400

    removi o arquivo anterior e tentarei do 0

--- SL/conversao_de_bases.py
+++ SL/conversao_de_bases.py
@@ -1,48 +1,25 @@
-quantidade, base_origem, base_final = input().split()
-
-quantidade = int(quantidade)
-
-# Função que checa se a base foi informada ou não, se naão for
-# Define a base padrão para 10.
-def checkBase(base_origem, base_final):
-    baseO = 0
-    baseF = 0
-
-
-    if base_origem == ' ' and base_final != ' ':
-        baseO = 10
-    elif base_final == ' ' and base_origem != ' ':
-        baseF = 10
-    else:
-        baseO = int(base_origem)
-        baseF = int(base_final)
-    return baseO, baseF
-
-base_origem = checkBase(base_origem,base_final)[0]
-base_final = checkBase(base_origem,base_final)[1]
-
+quantidade = input()
+base_original = input()
+base_final = input()
 valor_base10 = 0
 valor_base_final = ''
 
-# Transformando qualquer outra base para base 10
-if base_origem == 10 and base_final == 10:
-        valor_base10 = quantidade
-elif base_origem == 10:
-        valor_base10 = quantidade
+
+
+if base_final == '' and base_final == '':
+    base_original = 10
+    base_final = 10
+    valor_base_final = quantidade
+elif base_original == '':
+    base_original = 10
+elif base_final == '':
+    base_final = 10
 else:
-    for i, digito in enumerate(reversed(str(quantidade))):
-        valor_base10 += int(digito) * (base_origem ** i)
+    base_final = int(base_final)
+    base_original = int(base_original)
 
-temp = valor_base10
-
-
-if base_final != 0:
-    while temp != 0:
-        valor_base_final += str(temp % base_final)
-        temp = temp // base_final
-
-valor_base_final = valor_base_final[::-1]
-
-print(valor_base10)
-print(int(valor_base_final))
-
+if base_final != 10:
+    for i, digito in enumerate(reversed(quantidade)):
+        valor_base10 += int(digito) * (base_original ** i)
+else:
+    
