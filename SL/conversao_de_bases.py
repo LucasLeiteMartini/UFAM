ab619b163cd4f8565bc9d228a2b6e6dedb3a518d
Martini <lucas.leite.martini@gmail.com>
2023-04-03 11:32:28 -0400

    atualização código de conversão de bases

--- SL/conversao_de_bases.py
+++ SL/conversao_de_bases.py
@@ -2,42 +2,46 @@ quantidade, base_origem, base_final = input().split()
 
 quantidade = int(quantidade)
 
+# Função que checa se a base foi informada ou não, se naão for
+# Define a base padrão para 10.
+def checkBase(base_origem, base_final):
+    baseO = 0
+    baseF = 0
 
-def checkpadrao(base_origem, base_final):
-    
-    if base_origem == ' ':
-        base_origem = 10
-    elif base_final == ' ':
-        base_final = 10
+
+    if base_origem == ' ' and base_final != ' ':
+        baseO = 10
+    elif base_final == ' ' and base_origem != ' ':
+        baseF = 10
     else:
-        base_final = int(base_final)
-        base_origem = int(base_origem)
-    return base_final, base_origem
+        baseO = int(base_origem)
+        baseF = int(base_final)
+    return baseO, baseF
 
-checkpadrao(base_origem, base_final)
+base_origem = checkBase(base_origem,base_final)[0]
+base_final = checkBase(base_origem,base_final)[1]
 
-print(base_origem)
-print(base_final)
-
-'''
 valor_base10 = 0
 valor_base_final = ''
 
 # Transformando qualquer outra base para base 10
-if base_origem and base_final == 10:
-    for i, digito in enumerate(reversed(str(quantidade))):
-        valor_base10 += int(digito) * (base_origem ** i)
+if base_origem == 10 and base_final == 10:
+        valor_base10 = quantidade
+elif base_origem == 10:
+        valor_base10 = quantidade
 else:
     for i, digito in enumerate(reversed(str(quantidade))):
         valor_base10 += int(digito) * (base_origem ** i)
 
-#  Transformando base 10 para qualquer outra base
-while valor_base10 != 0:
-    valor_base_final += str(valor_base10 % base_final)
-    valor_base10 = valor_base10 // base_final
+temp = valor_base10
 
 
-valor_base10 = valor_base10 // base_final
+if base_final != 0:
+    while temp != 0:
+        valor_base_final += str(temp % base_final)
+        temp = temp // base_final
+
+valor_base_final = valor_base_final[::-1]
 
 print(valor_base10)
-print(valor_base_final)'''
+print(int(valor_base_final))
