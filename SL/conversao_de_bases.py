1592dea3059c6ecd9bbe1138dd328097a4c5faed
Martini <lucas.leite.martini@gmail.com>
2023-04-04 09:53:37 -0400

    modificações na lógica do programa

--- SL/conversao_de_bases.py
+++ SL/conversao_de_bases.py
@@ -2,6 +2,12 @@ quantidade = input()
 base_origem = input()
 base_final = input()
 
+'''if (letra >= 65 and letra <= 90):
+    numero = int(letra) - 55
+
+if (numero >= 10 and numero <= 35):
+    letra = chr(numero + 55)
+'''
 if base_origem == '' and base_final == '':
     base_origem = 10
     base_final = 10
@@ -15,7 +21,7 @@ else:
 
 valor_base10 = 0
 
-if base_origem and base_final == 0:
+if base_origem == 10 and base_final == 10:
     valor_base10 = int(quantidade)
 elif base_origem == 10:
     valor_base10 = int(quantidade)
@@ -23,8 +29,16 @@ else:
     for i, digito in enumerate(reversed(quantidade)):
         valor_base10 += int(digito) * (base_origem ** i)
 
-teste = []
+temp = valor_base10
+
+valor_base_final = ''
+
+while temp != 0:
+    valor_base_final += str(temp % base_final)
+    temp = temp // base_final
 
 
+
+print(valor_base_final[::-1])
 
 print(valor_base10)
