531498fd62bcd40557d7df95b5043060f3af9c72
Martini <lucas.leite.martini@gmail.com>
2023-05-25 22:21:16 -0400

    correcao do codigo questao01

--- AED1/aulas/listas_encadeadas/listatreino.c
+++ AED1/aulas/listas_encadeadas/listatreino.c
@@ -20,17 +20,24 @@ typedef struct tipoLista{
 void questao01(tipoLista *l, float chave){
     tipoNo *aux;
     tipoNo *tmp;
-    tmp = (tipoNo*) malloc(sizeof(tipoNo));
-    aux = l->prim;
-    tmp = aux;
 
-    while(aux){
-        if(aux->dado.altura == chave){
+    if(l->prim){
+        if(l->prim->dado.altura == chave){
+            aux = l->prim;
+            l->prim = aux->prox;
+            free(aux);
+        }else{
+            aux = l->prim;
+            while(aux->prox){
+                if(aux->prox != NULL && aux->prox->dado.altura != chave) aux = aux->prox;
+            }
+            if(aux->prox){
+            tmp = aux->prox;
+            aux->prox = tmp->prox;
             free(tmp);
+            }
         }
-        aux = aux->prox;
-        tmp = aux;
-    }
+    }    
 
 }
 
@@ -41,6 +48,7 @@ void questao02(tipoLista *l){
 
     while(aux){
         printf("%.2f", aux->dado.altura);
+        aux = aux->prox->prox;
     }
 }
 
@@ -58,4 +66,54 @@ void questao03(tipoLista *l, int tam){
             tamAux--;
         }
     }
+}
+
+void criaLista(tipoLista *l){
+    l->prim = NULL;
+}
+
+void insereNaLista(tipoLista *l, tipoPessoa info){
+
+    tipoNo *aux;
+
+    aux = (tipoNo*) malloc(sizeof(tipoNo));
+    aux->dado= info;
+    aux -> prox = l->prim;
+    l->prim = aux;
+}
+
+void mostraLista(tipoLista *l){
+    tipoNo *aux;
+
+    aux = l->prim;
+
+    while(aux){
+        printf("%s\n", aux->dado.nome);
+        printf("%s\n", aux->dado.endereco);
+        printf("%.2f\n", aux->dado.altura);
+        printf("\n");
+        aux = aux->prox;
+    }
+
+}
+
+int main(){
+    tipoLista l1;
+
+    criaLista(&l1);
+
+    tipoPessoa p1 = {"carlos","rua12",1.89};
+    tipoPessoa p2 = {"lucas","rua432",1.75};
+    tipoPessoa p3 = {"maria","rua553",1.55};
+    tipoPessoa p4 = {"roberto","rua7",1.94};
+
+    insereNaLista(&l1,p1);
+    insereNaLista(&l1,p2);
+    insereNaLista(&l1,p3);
+    insereNaLista(&l1,p4);
+
+    questao01(&l1,1.74);
+
+    mostraLista(&l1);
+
 }
